30879a3bead15f688996545ebc8c834d56ee8dcf
suppaya <suppaya32353@gmail.com>
2026-09-09 15:39:00 +0700

    feat: add tailwind styles and css types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "./globals.css"; // <-- เพิ่มบรรทัดนี้เพื่อดึง Tailwind CSS มาใช้
 import SessionProvider from "@/components/SessionProvider";
 
 export const metadata: Metadata = {
